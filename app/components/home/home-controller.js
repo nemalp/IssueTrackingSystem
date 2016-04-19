6058450c986f0bca150785685981d030a3f7b397
nemalp <lazarov.pl@gmail.com>
2016-04-19 22:46:13 +0300

    Add login and register user functions

--- app/components/home/home-controller.js
+++ app/components/home/home-controller.js
@@ -1,16 +1,50 @@
 (function() {
-    'use strict'
+    'use strict';
 
-    function HomeController() {
+    angular.module('issueTrackingSystem.home', [])
+        .config(config)
+        .controller('HomeController', HomeController);
 
-        function registerUser() {
-        
+    HomeController.$inject = ['authentication', '$http'];
+    config.$inject = ['$routeProvider'];
+
+    function HomeController(authentication, $http) {
+        var vm = this;
+
+        vm.register = register;
+        vm.login = login;
+
+        function register(user) {
+            authentication.register(user)
+                .then(
+                    function(res) {
+                        console.log(res.data);
+                        console.log(res);
+                    },
+                    function(err) {
+                        console.log(err);
+                });
         }
 
+        function login(user) {
+            authentication.login(user)
+                .then(
+                    function(res) {
+                        console.log(res);
+                    },
+                    function(err) {
+                        console.log(err);
+                    }
+                );
+        }
     }
 
-    angular.module('issueTrakingSystem.home' [])
-        // Add [] syntax for minification
-        .controller(HomeController);
+    function config($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'app/components/home/home-view.html',
+            controllerAs: 'vm',
+            controller: 'HomeController'
+        });
+    }
 
 }());
