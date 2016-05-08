a1b126c832eb684a85f6ca3a7b13599c504d4a15
nemalp <lazarov.pl@gmail.com>
2016-05-08 13:23:06 +0300

    Add change password

--- app/core/main.controller.js
+++ app/core/main.controller.js
@@ -2,14 +2,17 @@
     'use strict';
 
     angular.module('app.main')
+        .config(config)
         .controller('MainController', MainController);
 
     MainController.$inject = ['$http', '$location', 'authentication'];
+    config.$inject = ['$routeProvider'];
 
     function MainController($http, $location, authentication) {
         var vm = this;
 
         vm.logout = logout;
+        vm.changePassword = changePassword;
         vm.isAuthenticated = authentication.isAuthenticated;
 
         function logout() {
@@ -17,13 +20,36 @@
                 .then(
                     function() {
                         sessionStorage.clear();
-                        $location.path('/');
+                        $location.path('/logout');
                     },
                     function(error) {
                         console.log(error);
                     }
                 );
         }
+
+        function changePassword(user) {
+            authentication.changePassword(user)
+                .then(
+                    function(response) {
+                        console.log(response);
+                        $location.path('/');
+                    },
+
+                    function(error) {
+                        console.log(error);
+                    }
+                );
+        }
+    }
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/profile/password', {
+                templateUrl: 'app/core/templates/change-password.html',
+                controller: 'MainController',
+                controllerAs: 'vm'
+            })
     }
 
 
