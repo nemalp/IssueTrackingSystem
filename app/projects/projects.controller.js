aa199335fafa3bf56048256ff18e5a855a6fd796
nemalp <lazarov.pl@gmail.com>
2016-05-08 09:43:25 +0300

    Add additional logic and routes

--- app/projects/projects.controller.js
+++ app/projects/projects.controller.js
@@ -5,21 +5,35 @@
         .config(config)
         .controller('ProjectsContoller', ProjectsContoller);
 
-    ProjectsContoller.$inject = ['$http', '$location', 'projectsService', 'authentication'];
+    ProjectsContoller.$inject = ['$http', '$location', '$routeParams', 'projectsService', 'authentication'];
     config.$inject = ['$routeProvider'];
 
-    function ProjectsContoller($http, $location, projectsService, authentication) {
+    function ProjectsContoller($http, $location, $routeParams, projectsService, authentication) {
         var vm = this;
 
+        vm.getAllProjects = getAllProjects;
+        vm.addProject = addProject;
         vm.isAuthenticated = authentication.isAuthenticated();
+        vm.isAdmin = authentication.isAdmin;
 
         vm.projectsParams = {
             pageSize: 5,
             pageNumber: 1
         };
 
+        vm.usersDataSource = {
+            transport: {
+                read: {
+                    url: "http://softuni-issue-tracker.azurewebsites.net/users/",
+                    headers: {
+                        Authorization: 'Bearer ' + sessionStorage.authToken
+                    }
+                }
+            }
+        };
+
         if(vm.isAuthenticated) {
-            vm.getAllProjects = getAllProjects();
+            getAllProjects();
         }
 
         function getAllProjects() {
@@ -33,11 +47,37 @@
                 });
         }
 
+        function addProject(project) {
+            projectsService.addProject(project)
+                .then(
+                    function(response) {
+                        console.log(response);
+                        $location.path('projects/' + response.data.Id);
+                    },
+
+                    function(error) {
+                        console.log(error);
+                    }
+                );
+        }
     }
 
     function config($routeProvider) {
-        $routeProvider.when('/projects', {
+        $routeProvider
+        .when('/projects', {
             templateUrl: 'app/projects/templates/allProjects.html',
+            controller: 'ProjectsContoller',
+            controllerAs: 'vm'
+        })
+
+        .when('/projects/add', {
+            templateUrl: 'app/projects/templates/addProject.html',
+            controller: 'ProjectsContoller',
+            controllerAs: 'vm'
+        })
+
+        .when('/projects/:id', {
+            templateUrl: 'app/projects/templates/project-page.html',
             controller: 'ProjectsContoller',
             controllerAs: 'vm'
         });
